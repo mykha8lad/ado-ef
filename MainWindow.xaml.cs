f58348481d1504e3dab8015ab9bb651a907d6a21
Vladyslav Mykhailichenko <mykhailichenkovlad@gmail.com>
2023-10-09 17:46:35 +0300

    Update-9-10-23-2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ADO_EF_P12.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -44,13 +45,13 @@ public partial class MainWindow : Window
         TopChiefCountLabel.Content =
                 dataContext
                 .Managers
-                .Where(manager => manager.IdChief == null)                   
+                .Where(manager => manager.IdChief == null)
                 .Count()
                 .ToString();
         Subordinate.Content = dataContext.Managers
             .Where(manager => manager.IdChief != null)
             .Count()
-            .ToString();        
+            .ToString();
         Guid itEmployee = Guid
             .Parse(dataContext.Departments
             .Where(department => department.Name == "IT відділ")
@@ -73,42 +74,42 @@ public partial class MainWindow : Window
         var query = dataContext
                .Managers
                .Where(m => m.IdMainDep == Guid.Parse("131ef84b-f06e-494b-848f-bb4bc0604266"))
-               .Select(                       
-                   m =>                       
-                   new Pair                   
-                   {                          
-                       Key = m.Surname,       
+               .Select(
+                   m =>
+                   new Pair
+                   {
+                       Key = m.Surname,
                        Value = $"{m.Name[0]}. {m.Secname[0]}."
                    }
                )
                .Take(7);
-        
-        
+
+
 
         Pairs.Clear();
         foreach (var pair in query)
-        {                          
+        {
             Pairs.Add(pair);
         }
     }
 
     private void Button2_Click(object sender, RoutedEventArgs e)
     {
-        var query = dataContext           
-                .Managers                 
-                .Join(                    
+        var query = dataContext
+                .Managers
+                .Join(
                     dataContext.Departments,
-                    m => m.IdMainDep,       
-                    d => d.Id,              
-                    (m, d) =>               
-                       new Pair             
-                       {                    
-                           Key = m.Surname, 
-                           Value = d.Name   
+                    m => m.IdMainDep,
+                    d => d.Id,
+                    (m, d) =>
+                       new Pair
+                       {
+                           Key = m.Surname,
+                           Value = d.Name
                        }
-                )     
+                )
                 .Skip(3)
-                .Take(7);                  
+                .Take(7);
 
         Pairs.Clear();
         foreach (var pair in query)
@@ -150,8 +151,11 @@ public partial class MainWindow : Window
                     m => m.CreateDt
                 )
                 .Select(
-                    m => new Pair { Key = $"{m.CreateDt}",
-                    Value = $"{m.Surname} {m.Name[0]}.{m.Secname[0]}." }
+                    m => new Pair
+                    {
+                        Key = $"{m.CreateDt}",
+                        Value = $"{m.Surname} {m.Name[0]}.{m.Secname[0]}."
+                    }
                 )
                 .Take(7);
 
@@ -174,6 +178,180 @@ public partial class MainWindow : Window
             )
             .Take(7)
             .OrderBy(pair => pair.Value);
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    #region Генератор - збільшується при кожному зверненні
+    private int _N;
+    public int N { get => _N++; set => _N = value; }
+    #endregion
+    private void Button6_Click(object sender, RoutedEventArgs e)
+    {
+        N = 1;
+        var query = dataContext
+                .Departments
+                .OrderBy(d => d.Name)
+                .Select(d => new Pair()
+                {
+                    Key = (N).ToString(),
+                    Value = d.Name
+                });
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    private void Button7_Click(object sender, RoutedEventArgs e)
+    {
+        N = 1;
+        var query = dataContext
+            .Departments
+            .OrderBy(d => d.Name)
+            .AsEnumerable()
+            .Select(d => new Pair()
+            {
+                Key = (N).ToString(),
+                Value = d.Name
+            });
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    private void Button8_Click(object sender, RoutedEventArgs e)
+    {
+        var query = dataContext
+                .Departments
+                .GroupJoin(
+                    dataContext.Managers,
+                    d => d.Id,
+                    m => m.IdMainDep,
+                    (dep, mans) => new Pair
+                    {
+                        Key = dep.Name,
+                        Value = mans.Count()
+                                .ToString()
+                    }
+                );
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    private void Button9_Click(object sender, RoutedEventArgs e)
+    {
+        var query = dataContext.Managers
+                .GroupJoin(
+                    dataContext.Managers,
+                    chef => chef.Id,
+                    sub => sub.IdChief,
+                    (chef, subs) => new Pair()
+                    {
+                        Key = $"{chef.Surname} {chef.Name[0]}. {chef.Secname[0]}.",
+                        Value = subs.Count().ToString()
+                    }
+                )
+                .Where(p => Convert.ToInt32(p.Value) > 0);
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    private void Button10_Click(object sender, RoutedEventArgs e)
+    {
+        var query = dataContext.Managers
+                .GroupBy(m => m.Surname)
+                .Select(group => new Pair
+                {
+                    Key = group.Key,
+                    Value = group.Count().ToString()
+                })
+                .Where(p => Convert.ToInt32(p.Value) > 1);
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    private void Button11_Click(object sender, RoutedEventArgs e)
+    {
+        var query = dataContext
+            .Departments
+            .GroupJoin(
+                dataContext.Managers,
+                d => d.Id,
+                m => m.IdSecDep,
+            (d, m) => new Pair
+            {
+                Key = d.Name,
+                Value = m.Count().ToString()
+            })
+            .OrderByDescending(pair => Convert.ToInt32(pair.Value));
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    private void Button12_Click(object sender, RoutedEventArgs e)
+    {
+        N = 1;
+
+        var query = dataContext
+            .Managers
+            .GroupBy(m => m.Surname)
+            .AsEnumerable()
+            .Where(m => m.Count() > 1)
+            .Select(
+                group => new Pair 
+                {
+                    Key = N.ToString(),
+                    Value = group.Key
+                });
+
+        Pairs.Clear();
+        foreach (var pair in query)
+        {
+            Pairs.Add(pair);
+        }
+    }
+
+    private void Button13_Click(object sender, RoutedEventArgs e)
+    {
+        var query = dataContext
+            .Managers
+            .GroupJoin(
+                dataContext.Managers,
+                chief => chief.Id,
+                m => m.IdChief,
+                (chief, m) => new Pair
+                {
+                    Key = m.Count().ToString(),
+                    Value = $"{chief.Surname} {chief.Name[0]}.{chief.Secname[0]}."
+                })
+                .OrderByDescending(pair => Convert.ToInt32(pair.Key))
+                .Take(3);
 
         Pairs.Clear();
         foreach (var pair in query)
